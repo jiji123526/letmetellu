69c2752e63cf848f145690ed335c3c364cb37f5f
jjiwoo <jjiwoo@amazon.com>
2026-08-19 18:28:47 +0000

    new feature announcement dialog deleted

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,14 +3,12 @@
 import { SessionProvider } from "next-auth/react";
 import { LocaleProvider } from "@/hooks/useLocale";
 import { UserPreferencesSync } from "@/components/UserPreferencesSync";
-import { ProductUpdateDialog } from "@/components/ProductUpdateDialog";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <UserPreferencesSync />
       <LocaleProvider>
-        <ProductUpdateDialog />
         {children}
       </LocaleProvider>
     </SessionProvider>
